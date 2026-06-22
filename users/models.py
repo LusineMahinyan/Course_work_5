0e9a812aa1c5d4e7f2bb63fe026a1e4a87477dc0
Лусине <lumahinyan@mail.ru>
2026-06-22 20:55:21 +0300

    Create custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    telegram_chat_id = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name='Telegram chat id'
+    )
+
+    def __str__(self):
+        return self.username
